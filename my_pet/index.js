23825d0e45db2fdf88bf19917105b8fa706c90d8
Iuliia-Bogdanova <u-bogdanova@yandex.ru>
2022-10-14 20:47:47 +0400

    home task

---
+++ my_pet/index.js
@@ -0,0 +1,10 @@
+function clickMe() {
+    let catNameInput = document.getElementById('catName');
+    console.log(catNameInput.value);
+}
+
+function changeMe() {
+    let catImage = document.getElementById ('catImage');
+    catImage.src = "(3).jpg";
+}
+
